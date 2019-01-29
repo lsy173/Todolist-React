440f0ccc68d56c4e4e4297cb2852f1d105f15fc2
이수영 <ssoo6247@gmail.com>
2019-01-29 18:19:36 +0900

    TodoItem Component created.

---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styles from './TodoItem.scss';
+import className from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+class TodoItem extends Component {
+    render () {
+        const {done, children, onToggle, onRemove} = this.props;
+
+        return (
+            <div className={cx('todo-item')} onClick={onToggle} >
+              <input className={cx('tick')} type="checkbox" checked={done} readOnly/>
+              <div className={cx('text', {done})}>{children}</div>
+              <div className={cx('delete')} onClick={onRemove}>[Erase]</div>
+            </div>
+        );
+    }
+}
+
+export default TodoItem;
